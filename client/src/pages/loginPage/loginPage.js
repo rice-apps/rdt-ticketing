6c903f0156a25721e946af914c2eeac6640a6d48
nhi-quach <nhi.quach.149@gmail.com>
2024-02-24 14:22:46 -0600

    yay

---
+++ client/src/pages/loginPage/loginPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./loginPage.css"
+
+import Logo from "./images/logo.png"
+import LoginBackground from "./images/login.png"
+import Google from "./images/google.png"
+
+function Login(){
+    const background = [{
+        LoginBackground
+    }]
+
+const img = document.querySelector("login")
+
+
