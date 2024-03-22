55e1b26923ee45a02c30b8c0d31e06408e9171d8
Tayten Bennetsen <youremail@gmail.com>
2024-03-22 10:17:29 -0500

    EventCard

--- client/src/components/Eventcard.js
+++ client/src/components/Eventcard.js
@@ -1,1 +1,22 @@
 import React from "react";
+
+import "../styles/ComponentsEventCard.css"
+
+const EventCard = (props) => {
+  return (
+    <div className="event">
+          <div className="event-image">
+            {props.photo}
+          </div>
+          <div className="event-description">
+            <div className="event-name"><h2>{props.name}</h2></div>
+            <h3>Date: {props.date}</h3>
+            <h3>Time: {props.startTime} - {props.endtime}</h3>
+            <h3>Location: {props.location}</h3>
+          </div>
+          
+    </div>
+  )
+}
+
+export default EventCard
