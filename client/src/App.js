be0edf3f15a7a329d3650f147f04ba81dd76e48d
Tayten Bennetsen <youremail@gmail.com>
2024-03-23 15:04:12 -0500

    EventCard

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import EventCard from "./components/Eventcard.js";
 function App() {
   return (
     <div className="App">
-      <EventCard name ={"Event 1"} date = {"3/23/2024"} startTime = {"6:30"} endTime = {"8:30"} image = {"https://picsum.photos/200/100"} location = {"Somewhere"}/>
+      <EventCard name ={"Event 1"} date = {"3/23/2024"} startTime = {"6:30"} endTime = {"8:30"} image = {"https://picsum.photos/340/159"} location = {"Somewhere"}/>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import LoginPage from "./pages/loginPage/LoginPage.js"
+import LoginPage from "./pages/loginPage/loginPage.js"
 import Footer from './components/Footer.js'
 import reportWebVitals from './reportWebVitals';
 
