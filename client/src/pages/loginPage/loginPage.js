62d59e8e4b9d3a9394ccd2884d28e2b3dc81daa6
R Kilgard <rkilgard@Rs-MacBook-Pro-2.local>
2024-03-23 14:59:27 -0500

    Register Event

--- client/src/pages/loginPage/loginPage.js
+++ client/src/pages/loginPage/loginPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./LoginPage.css"
+import "./loginPage.css"
 
 const LoginPage = () => {
 
---
+++ client/src/pages/registerEvent/registerEvent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./registerEvent.css"
+
+const RegisterPage = () => {
+
+
+  return (
+    <div className="Information">
+        <div className = 'FullName'></div>
+        <div className = 'Email'></div>
+        <div className='Quantity'></div>
+    </div>
+  );
+}
+
+export default LoginPage;
