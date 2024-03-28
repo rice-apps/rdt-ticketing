c9b1bc14014fc2cd083f2091ce5694cec3a88d7e
Anthony Yan <ady2@rice.edu>
2024-03-27 21:02:51 -0500

    workingish

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ function App() {
   }
   return (
     <div className="App">
-      <button onClick={login}>login</button>
+      {/* <button onClick={login}>login</button> */}
       <EventCard name ={"Event 1"} date = {"3/23/2024"} startTime = {"6:30"} endTime = {"8:30"} image = {"https://picsum.photos/200/100"} location = {"Somewhere"}/>
       <SeatingChart />
       {/* < SeatsioSeatingChart
--- client/src/components/SeatingChart.js
+++ client/src/components/SeatingChart.js
@@ -6,26 +6,24 @@ const SeatingChart = () => {
     const PUBLIC_WORKSPACE_KEY = "ab92e245-48bb-4d3d-856a-90b1fc8adadd"
     const EVENT_DAY_1_KEY = "ef47e713-1139-4e0b-bcbc-97de1331b06d"
 
-
-
     const pricing = [
         { category: 'Floor', price: 5 },
     ]
 
   return (
-    <div>
+    <div style={{"height": "500px"}}>
           < SeatsioSeatingChart
               workspaceKey="ab92e245-48bb-4d3d-856a-90b1fc8adadd"
               event="ef47e713-1139-4e0b-bcbc-97de1331b06d"
-            //   pricing={pricing}
-              region="eu"
+              region="na"
+            //   colorScheme="dark"
           />
 
-          <SeatsioEventManager
+          {/* <SeatsioEventManager
               secretKey="6e446b5d-c0d9-48d4-b7ce-53045869b4d1"
               event="ef47e713-1139-4e0b-bcbc-97de1331b06d"
               region="eu"
-          />
+          /> */}
 
     </div>
   )
